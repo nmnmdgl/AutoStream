e4ea1c2c91debaf74e22b633dcfa38752e182b00
NAMAN MUDGAL <126274325+nmnmdgl@users.noreply.github.com>
2026-01-14 13:50:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCNdCVHytAlQgfWIUQ4Q_l_2d8XVH-kzTU")
+genai.configure(api_key="YOUR_API_KEY")
 
 for m in genai.list_models():
     print(m.name)
